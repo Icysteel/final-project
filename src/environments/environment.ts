f33663b68cb60a65b3765c71d03bd03d5bea7d5b
Miroslav <icysteel1@gmail.com>
2019-01-13 12:00:24 +0200

    added firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAWEkjpjkRxxbVdL31n8FBsmMcdwcd9LbI',
+    authDomain: 'final-project-e12b3.firebaseapp.com',
+    databaseURL: 'https://final-project-e12b3.firebaseio.com',
+    projectId: 'final-project-e12b3',
+    storageBucket: 'final-project-e12b3.appspot.com',
+    messagingSenderId: '1001223241156'}
 };
 
 /*
