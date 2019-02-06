50d3558a48ff58834e4c051d77150c5f93a72530
Nayden <soodker@gmail.com>
2019-02-06 03:25:47 +0200

    modal for selecting devices

--- Frontend/src/app/devices/devices-list/devices-list.component.ts
+++ Frontend/src/app/devices/devices-list/devices-list.component.ts
@@ -2,6 +2,7 @@ import { DevicesService } from './../devices.service';
 import { Component, OnInit, ViewChild, EventEmitter, Output, Input } from '@angular/core';
 
 import { МodalComponent } from 'src/app/shared/modal/modal.component';
+import { distinct } from 'rxjs/operators';
 
 
 @Component({
@@ -11,36 +12,52 @@ import { МodalComponent } from 'src/app/shared/modal/modal.component';
 export class DevicesListComponent implements OnInit {
   constructor(private readonly devicesService: DevicesService) { }
 
-  @ViewChild(МodalComponent) public modal: МodalComponent;
-  @Input() public title: string;
-  @Output() public getPostTitle = new EventEmitter<string>();
-
-
   devices: any[];
   devicesNames = [];
-
+  selectedDevices = [];
   table: any[];
+  show = true;
+  ngReady = false;
 
   ngOnInit() {
-    const getArray = [];
-    let params = '';
+    const params = '';
     this.devicesService.getAllDevices().subscribe(data => {
       this.devices = data;
+      console.log('devices');
+      console.log(this.devices);
+
+
 
       this.devices.forEach(dev => this.devicesNames.push(dev.name));
-      this.devices.forEach(dev => getArray.push(dev.name));
-      params = getArray.join(',');
-      this.devicesService.getFromApi(params).subscribe(data1 => {
-        const arrayFromDevices = Object.entries(data1);
-        this.table = arrayFromDevices;
-        console.log(this.devicesNames);
-        console.log('table:');
-
-        console.log(this.table);
-      });
 
     });
 
   }
 
+  compareTimes(device) {
+    this.selectedDevices.push(device);
+    const i = this.devicesNames.indexOf(device);
+    this.devicesNames.splice(i, 1);
+  }
+
+  removeDevice(device) {
+    this.devicesNames.push(device);
+    const i = this.selectedDevices.indexOf(device);
+    this.selectedDevices.splice(i, 1);
+
+  }
+
+  callNg() {
+    this.CallAPI();
+    this.ngReady = true;
+  }
+
+  CallAPI() {
+    const params = this.selectedDevices.join(',');
+    this.devicesService.getFromApi(params).subscribe(data1 => {
+      const arrayFromDevices = Object.entries(data1);
+      this.table = arrayFromDevices;
+    });
+    console.log(this.selectedDevices);
+  }
 }
